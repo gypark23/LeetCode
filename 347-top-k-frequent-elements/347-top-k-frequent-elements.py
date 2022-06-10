f631ff98dc6fa5b5610a0faa9154c75883e99b73
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-10 08:36:55 -0400

    Time: 110 ms (84.29%), Space: 19.3 MB (25.92%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
         ret = []
         dic = {}
         for num in nums:
@@ -14,3 +15,19 @@ class Solution:
             ret.append(dic_list[i][0])
         
         return ret
+        """
+        
+        ret = []
+        dic = [[] for i in range(len(nums) + 1)]
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        for n, c in count.items():
+            dic[c].append(n)
+         
+        for i in range(len(dic) - 1, -1, -1):
+            for n in dic[i]:
+                ret.append(n)
+                if(len(ret) == k):
+                    return ret
