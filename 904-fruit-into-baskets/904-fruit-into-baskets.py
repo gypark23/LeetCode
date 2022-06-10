254b3623da0225c27e2454543e69f66faef3c45f
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-10 10:19:37 -0400

    Time: 1862 ms (7.85%), Space: 20 MB (95.48%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -3,14 +3,17 @@ class Solution:
         dic = {}
         low = 0
         maxsum = 0
+        _sum = 0
         for high in range(len(fruits)):
             dic[fruits[high]] = 1 + dic.get(fruits[high], 0)
+            _sum += 1
             while(len(dic) > 2):  
                 if(dic[fruits[low]] > 1):
                     dic[fruits[low]] -= 1
                 else:
                     del dic[fruits[low]]
                 low += 1
-            maxsum = max(maxsum, sum(dic.values()))
+                _sum -= 1
+            maxsum = max(maxsum, _sum)
         
         return maxsum
