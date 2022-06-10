b18faf18fb2f51ca51745191128806b93975921d
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-10 09:47:02 -0400

    Time: 59 ms (23.16%), Space: 13.8 MB (62.80%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        """count sort"""
+        """count sort
         count = [0] * 3
         for num in nums:
             count[num] += 1
@@ -16,3 +16,29 @@ class Solution:
                 j += 1
                 count[i] -= 1
             i += 1
+        """
+        
+        """three pointers"""
+        
+        def swap(a, b):
+            temp = nums[a]
+            nums[a] = nums[b]
+            nums[b] = temp
+        
+        low, mid, high = 0, 0, len(nums) - 1
+        
+        while(mid <= high):
+            if(nums[mid] == 0):
+                swap(mid, low)
+                low += 1
+                mid += 1
+            elif(nums[mid] == 2):
+                swap(mid, high)
+                high -= 1
+            else:
+                mid += 1
+            
+            
+        
+        
+            
