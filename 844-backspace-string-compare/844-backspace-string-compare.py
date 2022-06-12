dc51d43f0bd73d1f3f3708e7f8f16e43263f6253
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-12 18:50:14 -0400

    Time: 73 ms (5.43%), Space: 14 MB (22.72%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,26 +1,44 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stack_s = [0] * 200
-        stack_t = [0] * 200
-        idx_s, idx_t = 0, 0
+        sptr, tptr = len(s) - 1, len(t) - 1
+        sjump, tjump = 0, 0
+        while(sptr >= 0 or tptr >= 0):
+            while(sptr >= 0):
+                if(s[sptr] == '#'):
+                    sjump += 1
+                    sptr -= 1
+                else:
+                    break
+            while(tptr >= 0):
+                if(t[tptr] == '#'):
+                    tjump += 1
+                    tptr -= 1
+                else:
+                    break
+            while(sjump > 0):
+                sptr -= 1
+                if(sptr < 0):
+                    break
+                if(s[sptr] == '#'):
+                    sjump += 1
+                else:
+                    sjump -= 1
+            while(tjump > 0):
+                tptr -= 1
+                if(tptr < 0):
+                    break
+                if(t[tptr] == '#'):
+                    tjump += 1
+                else:
+                    tjump -= 1
         
-        for char in s:
-            if(char == '#'):
-                idx_s -= 1
-                idx_s = max(0, idx_s)
-                stack_s[idx_s] = 0
-            else:
-                stack_s[idx_s] = char
-                idx_s += 1
-        
-        for char in t:
-            if(char == '#'):
-                idx_t -= 1
-                idx_t = max(0, idx_t)
-                stack_t[idx_t] = 0
-            else:
-                stack_t[idx_t] = char
-                idx_t += 1
-                
-
-        return stack_s == stack_t
+            if(not (sptr >= 0) ^ (tptr < 0)):
+                return False
+            if(sptr >= 0 and tptr >= 0 and s[sptr] != t[tptr]):
+                #print(sptr, tptr, s[sptr], t[tptr])
+                return False
+            
+            sptr -= 1
+            tptr -= 1
+            
+        return True
