c5d468ca3d2b2dfaefc26fcd8aa0d37ab990940a
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-12 19:07:50 -0400

    Time: 50 ms (35.88%), Space: 13.9 MB (22.72%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -2,43 +2,36 @@ class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         sptr, tptr = len(s) - 1, len(t) - 1
         sjump, tjump = 0, 0
+        
         while(sptr >= 0 or tptr >= 0):
-            while(sptr >= 0):
+            while(sptr >= 0 or sjump == 0):
                 if(s[sptr] == '#'):
                     sjump += 1
-                    sptr -= 1
+                elif(sjump > 0):
+                    sjump -= 1
                 else:
                     break
+                sptr -= 1
             while(tptr >= 0):
                 if(t[tptr] == '#'):
                     tjump += 1
-                    tptr -= 1
-                else:
-                    break
-            while(sjump > 0):
-                sptr -= 1
-                if(sptr < 0):
-                    break
-                if(s[sptr] == '#'):
-                    sjump += 1
-                else:
-                    sjump -= 1
-            while(tjump > 0):
-                tptr -= 1
-                if(tptr < 0):
-                    break
-                if(t[tptr] == '#'):
-                    tjump += 1
-                else:
+                elif(tjump > 0):
                     tjump -= 1
-        
-            if(not (sptr >= 0) ^ (tptr < 0)):
-                return False
-            if(sptr >= 0 and tptr >= 0 and s[sptr] != t[tptr]):
-                #print(sptr, tptr, s[sptr], t[tptr])
-                return False
+                else:
+                    break
+                tptr -= 1
             
+            if(tptr < 0 and sptr < 0):
+                return True
+            elif((tptr < 0 and sptr >= 0) or (tptr >= 0 and sptr < 0)):
+                return False
+            elif(t[tptr] != s[sptr]):
+                return False
+        
             sptr -= 1
             tptr -= 1
-            
+        
         return True
+            
+            
+            
