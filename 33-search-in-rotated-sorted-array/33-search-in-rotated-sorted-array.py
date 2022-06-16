7fab2eba75e5eada40888e125672da698ed29cb4
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-15 22:06:50 -0400

    Time: 45 ms (86.42%), Space: 14.5 MB (18.37%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -7,19 +7,18 @@ class Solution:
             print(low, mid, high)
             if(nums[mid] == target):
                 return mid
-            #any numbers after nums[mid] sorted
-            if(nums[low] <= nums[mid]):
-                if(target > nums[mid] or target < nums[low]):
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            
             #any numbers before nums[mid] sorted
-            else:
-                if(target < nums[mid] or target > nums[high]):
+            if(nums[low] <= nums[mid]):
+                if(target >= nums[low] and target <= nums[mid]):
                     high = mid - 1
                 else:
                     low = mid + 1
- 
+            #any numbers after nums[mid] sorted
+            #[7,-1,1,2,3]
+            else:
+                if(target <= nums[high] and target >= nums[mid]):
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return -1
                 
