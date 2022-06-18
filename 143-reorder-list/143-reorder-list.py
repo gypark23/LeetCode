1816353c26512bb5c29ad84c163bac34aa78eea8
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-17 21:54:51 -0400

    Time: 89 ms (94.83%), Space: 23.9 MB (95.65%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,6 +8,31 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        #find midpoint
+        mid, fast = head, head.next
+        while(fast and fast.next):
+            mid = mid.next
+            fast = fast.next.next
+        #reverse second half
+        curr = mid.next
+        prev = mid.next = None
+        _next = None
+        while(curr):
+            _next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = _next
+        mid = prev
+        
+        #merge
+        while(mid):
+            head_next, mid_next = head.next, mid.next
+            head.next = mid
+            mid.next = head_next
+            head, mid = head_next, mid_next
+        
+
+        """stack
         temp = head
         stack = []
         while(temp):
@@ -20,4 +45,4 @@ class Solution:
             else:
                 head.val = stack[-(i // 2 + 1)]
             head = head.next
-        
+       """ 
