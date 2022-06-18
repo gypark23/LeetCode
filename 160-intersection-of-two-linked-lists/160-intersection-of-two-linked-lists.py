6f14a068b079b6eb12218a5e81c996e033671ea3
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-06-17 20:53:24 -0400

    Time: 199 ms (67.22%), Space: 29.5 MB (69.60%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -6,6 +6,7 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        """hash table
         dic = {}
         
         while(headA):
@@ -15,5 +16,30 @@ class Solution:
             if(headB in dic):
                 return headB
             headB = headB.next
+        
+        return None
+        """
+        
+        """two pointer"""
+        
+        lenA, lenB = 0, 0
+        tempA, tempB = headA, headB
+        while(tempA):
+            lenA += 1
+            tempA = tempA.next
+        while(tempB):
+            lenB += 1
+            tempB = tempB.next
+        
+        long, short = headA if lenA >= lenB else headB, headB if lenA >= lenB else headA
+        
+        for i in range(abs(lenA - lenB)):
+            long = long.next
+        
+        while(long and short):
+            if(long == short):
+                return long
+            long = long.next
+            short = short.next
         
         return None
