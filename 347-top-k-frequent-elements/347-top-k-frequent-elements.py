1d888a9d6873967c1c1700c3f84d7cd5fa884d71
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-10 12:14:50 -0400

    Time: 146 ms (60.97%), Space: 18.6 MB (70.40%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,34 +1,5 @@
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        ret = []
-        dic = {}
-        for num in nums:
-            if num in dic:
-                dic[num] += 1
-            else:
-                dic[num] = 1
-        
-        dic_list = list(dic.items())
-        dic_list.sort(key = lambda x: x[1], reverse = True)
-        for i in range(k):
-            ret.append(dic_list[i][0])
-        
-        return ret
-        
-        """
-        ret = []
-        dic = [[] for i in range(len(nums) + 1)]
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        
-        for n, c in count.items():
-            dic[c].append(n)
-         
-        for i in range(len(dic) - 1, -1, -1):
-            for n in dic[i]:
-                ret.append(n)
-                if(len(ret) == k):
-                    return ret
-        """
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), count.get)
