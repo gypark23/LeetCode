21fa3fb74bd7d61f00d2a5e2d4392f0d1b8e6c23
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-15 12:12:32 -0400

    Time: 552 ms (32.57%), Space: 16.3 MB (90.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,27 +3,23 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ret = 0
-        def dfs(grid, r, c):
-            stack = deque()
-            stack.append((r, c))
-            directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
-            
-            while stack:
-                new_r, new_c = stack.pop()
-                grid[new_r][new_c] = "0"
-                for x, y in directions:
-                    new_r += x
-                    new_c += y
-                    if new_r in range(len(grid)) and new_c in range(len(grid[0])) and grid[new_r][new_c] == "1":
-                        stack.append((new_r, new_c))
-                    new_r -= x
-                    new_c -= y
-        
-        
+        queue = deque()
+        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == "1":
                     ret += 1
-                    dfs(grid, r, c)
+                    queue.append((r, c))
+                    
+                    while queue:
+                        new_r, new_c = queue.popleft()
+                        for x, y in directions:
+                            if new_r + x in range(len(grid)) and new_c + y in range(len(grid[0])) and grid[new_r + x][new_c + y] == "1":
+                                queue.append((new_r + x, new_c + y))
+                                grid[new_r + x][new_c + y] = "0"
         
         return ret
+                    
+        
+        
+        
