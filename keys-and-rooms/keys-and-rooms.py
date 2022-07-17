3ac8924f5991d3f51be1a89d50d28e7b0251efb7
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-17 09:54:07 -0400

    Time: 127 ms, Memory: 14.4 MB - LeetHub

---
+++ keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        queue = deque()
+        queue.append(0)
+        visited.add(0)
+        while queue:
+            room = queue.pop()
+            for key in rooms[room]:
+                if key not in visited:
+                    queue.append(key)
+                    visited.add(key)
+        
+        for i in range(len(rooms)):
+            if i not in visited:
+                return False
+        return True
