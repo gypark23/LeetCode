db6f81065dd98a3ab1ad71895f7b1af121f43360
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-16 20:59:58 -0400

    Time: 451 ms (57.36%), Space: 16.4 MB (80.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,25 +1,23 @@
 from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ret = 0
+        row, col = len(grid), len(grid[0])
         queue = deque()
-        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == "1":
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        ret = 0
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1":
                     ret += 1
-                    queue.append((r, c))
-                    
+                    queue.append((i, j))
                     while queue:
-                        new_r, new_c = queue.popleft()
+                        r, c = queue.popleft()
+                        grid[r][c] = "0"
                         for x, y in directions:
-                            if new_r + x in range(len(grid)) and new_c + y in range(len(grid[0])) and grid[new_r + x][new_c + y] == "1":
-                                queue.append((new_r + x, new_c + y))
-                                grid[new_r + x][new_c + y] = "0"
+                            new_r, new_c = r + x, c + y
+                            if new_r in range(row) and new_c in range(col) and grid[new_r][new_c] == "1":
+                                queue.append((new_r, new_c))
+                                grid[new_r][new_c] = "0"
         
         return ret
-                    
-        
-        
         
