7569936c6d8467e258dbae8cc46cc83ed85e5e92
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-22 11:55:46 -0400

    Time: 58 ms (52.59%), Space: 14.1 MB (93.71%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -10,9 +10,10 @@ class Solution:
             path.append(nums[i])
             dfs(i + 1, path)
             path.pop()
-            while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+            i += 1
+            while i < len(nums) and nums[i - 1] == nums[i]:
                 i += 1
-            dfs(i + 1, path)
+            dfs(i, path)
     
         dfs(0, [])
         return ret
