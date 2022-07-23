eb18cfd0c2ed3259e49ea2bff6037ebe5f829dbb
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-22 21:13:39 -0400

    Time: 746 ms (79.25%), Space: 30.4 MB (24.17%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,33 +1,33 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ret = []
-        
         def isPalindrome(low, high):
             while low <= high:
                 if s[low] != s[high]:
                     return False
                 low += 1
                 high -= 1
+            
             return True
         
-        def backtrack(start, end, path):
+        def backtrack(path, start, end):
             if start >= len(s):
                 ret.append(path)
                 return
+
             
             #option 1, split at end
             if isPalindrome(start, end - 1):
                 path.append(s[start:end])
-                backtrack(end, end + 1, path[:])
+                backtrack(path[:], end, end + 1)
                 path.pop()
             
             #option 2, don't split
             if end < len(s):
-                backtrack(start, end + 1, path[:])
-        
-        backtrack(0, 1, [])
+                backtrack(path[:], start, end + 1)
+            
+        backtrack([], 0, 1)
         return ret
-            
-            
+
             
             
