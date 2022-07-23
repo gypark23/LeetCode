57b7dfe6c257d74325c2d0af0edb3880aea149de
Kyu Park <68811862+gypark23@users.noreply.github.com>
2022-07-23 15:06:20 -0400

    Time: 29 ms (97.33%), Space: 13.9 MB (71.22%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -13,13 +13,11 @@ class Solution:
         #case 1, rob first house
         dp_1[0] = nums[0]
         dp_1[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums) - 1):
-            dp_1[i] = max(dp_1[i - 1], dp_1[i - 2] + nums[i])
-        
         #case 2, don't rob first house, start from second house
         dp_2[0] = nums[1]
         dp_2[1] = max(nums[1], nums[2])
         for i in range(2, len(nums) - 1):
+            dp_1[i] = max(dp_1[i - 1], dp_1[i - 2] + nums[i])
             dp_2[i] = max(dp_2[i - 1], dp_2[i - 2] + nums[i + 1])
         
         return max(dp_1[-1], dp_2[-1])
